0a9ecabe43afdaebc2b02161aba62a9f1783dbfe
Shaqeeb Momen <shaqeebmomen@users.noreply.github.com>
2018-02-27 14:56:57 -0500

    Initial Commit

---
+++ PlanetaryScript.m
@@ -0,0 +1,13 @@
+% Input Prompts for relevant values
+pitch = input("Pitch")
+ringDMax = input("Maximum size for the ring gear")
+planetCount = input("How many planet gears")
+ratio = input("Ratio you want to achieve")
+pAng = input("Pressure angle")
+
+% Note the gearbox will be in two stages with the the sun as the input and
+% planet carrier as the output for each stage
+
+
+% Known Constants
+HP = 7.5 %Horsepower of given engine
