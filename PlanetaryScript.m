d743534b88987fff32e71bdbd83180030e6955f0
Shaqeeb Momen <shaqeebmomen@users.noreply.github.com>
2018-03-20 20:08:37 -0400

    Creating Function to iterate and to check strength

--- PlanetaryScript.m
+++ PlanetaryScript.m
@@ -1,25 +1,30 @@
 % Input Prompts for relevant values
 pitch = input("Pitch");
-ringDMax = input("Maximum size for the ring gear");
+ringDMax = input("Maximum pitch diameter size for the ring gear");
 planetCount = input("How many planet gears");
 ratio = input("Ratio you want to achieve");
-zA = input("Sun tooth count");
+%%A = input("Sun tooth count");
 SIT = input("Shock Torque"); 
 %pAng = input("Pressure angle")
 
 % Note the gearbox will be in two stages with the the sun as the input and
 % planet carrier as the output for each stage
 
-%The convention followed for gears labes will be A:Sun, B:Planet, C:Ring
+%The convention followed for gears labels will be A:Sun, B:Planet, C:Ring
 
 % Known Constants
 HP = 7.5; %Horsepower of given engine
 pAng = 20; %Pressure Angle of gears
+Ys = 108000; %Yield strength of gears in Psi
 
+RingTMax = ringDMax * Pitch;
+zC = RingTmax;
 
 D_A = zA*pitch; %Pitch Dia of Sun
 D_B = zB*pitch; %Pitch Dia of Planet
 D_C = ZC*pitch; %Pitch Dia of Ring
+
+%Function to calculate stress on tooth
 
 %Stage One
 
@@ -28,9 +33,18 @@ IFt = SIT*D_B/2; %Inertial Forces Tangentially
 IFr =IFt*tan(pAng); %Inertial Forces Radially
 
  
-%Forces b/w A to B(Should be the same for contact forces b/w B to C from
-%Engine
-PV_A = pi*D_A*iRPM/12; %pitch line velocity in ft/min
-EFt = 33000*HP/PV_A; %Force tangential of sun to planet (Engine)
-EF = Ft*tan(pAng); %Force radial of sun to planet  (Engine)
+%Conteact forces b/w gears from engine
+PV_A = pi*D_A*iRPM/12; %pitch line velocity in ft/min 
+EFt = 33000*HP/PV_A; %Force tangential of sun to planet
+EF = Ft*tan(pAng); %Force radial of sun to planet
+
+
+while checkStrength(zA,zB,zC) == 1
+    zC = zC-1;
+    
+    
+    
+end
+
+
 
---
+++ checkStrength.m
@@ -0,0 +1,8 @@
+function [ strong ] = checkStrength( sun,planet,ring,Y,PA,FW,Ft,K)
+%checkStrength takes the sizes of the various gears and checks their
+%strength against known yield strength
+%   Detailed explanation goes here
+
+
+end
+
